e48c678020fe71e7da1d2dca5c06f6b29f5e3b74
Edward Ohemeng <34719083+ideas24@users.noreply.github.com>
2021-11-05 14:21:45 +0000

    Update uname -r

--- acron.sh
+++ acron.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 sudo apt-get update
-sudo apt-get installlinux -headers-`uname -r`
-sudo apt-get installlinux -image-`uname -r`
+sudo apt-get installlinux -headers-5.11.0-1020-azure
+sudo apt-get installlinux -image-5.11.0-1020-azure
 sudo apt-get install gcc-4.6
 sudo apt-get install make
